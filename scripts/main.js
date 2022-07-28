9b872b537c19033a2a4224fef52c2a9e31f78027
KonstantinEw <Ewwel@inbox.ru>
2022-07-28 20:43:13 +0300

    1-5 task

---
+++ scripts/main.js
@@ -0,0 +1,74 @@
+//1
+
+// function getSum(n) {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//         sum += i;
+//     }
+//     return console.log(sum);
+// }
+
+// getSum(100)
+
+//2
+
+// function getSum(sum) {
+//     let creditPersent = sum/100*17*5;
+//     return console.log (creditPersent);
+// }
+
+// getSum(2800);
+
+// function getSum(sum) {
+//     let creditPersent = 17
+//         creditYear = 5
+//         overPayment = sum/100*creditPersent*creditYear;
+//         return console.log(overPayment);
+// }
+
+// getSum(2800);
+
+//3
+
+// function trimString(str, sliceFrom, sliseTo) {
+//     return console.log (str.slice(sliceFrom, sliseTo));
+// }
+
+// trimString('Lorem ipsum dolor', 5);
+
+//4
+
+// function getSumNumbers (number) {
+//     let figures = "" + number;
+//     let sum = 0;
+//     for (let i = 0; i < figures.length; i++){
+//     sum += +figures[i];
+//     }
+//     return console.log(sum);
+// }
+
+//     getSumNumbers(255)
+
+//5
+
+// function getSum (a,b) {
+//     let min = Math.min(a,b);
+//     let max = Math.max(a,b);
+//     let sum = max;
+
+//     for (i=min; i<max; i++) {
+//         sum+=i;
+//         }
+//         return console.log(sum);
+// }
+
+// function getSum( a,b ) {
+//     let min = Math.min(a, b), max = Math.max(a, b);
+//     return console.log ((max - min + 1) * (min + max) / 2);
+// }
+
+// getSum(-1,1)
+
+//6
+
+
